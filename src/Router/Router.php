3bc4b6410f71f4fb80ab0fad4046510ea0d7e522
Grégory Moret <gregory.moret@outlook.fr>
2016-07-29 11:09:20 +0200

    add route method

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -9,20 +9,38 @@
 
 namespace DesignPatterns\Router;
 
+use DesignPatterns\Container\Container;
 use Interop\Container\ContainerInterface;
 
 class Router
 {
     private $config = array();
+    private $container;
 
     /**
      * Router constructor.
      * @param array $config
+     * @param mixed $container
      */
     public function __construct(array $config, ContainerInterface $container)
     {
         $this->config = $config;
+        $this->container = $container;
     }
 
-
+    public function route()
+    {
+        $route = $_GET['route'];
+        if (isset($this->config[$route]))
+        {
+            // charge service à partir du container
+            $controller = $this->container->get($this->config[$route]);
+            $controller->indexAction();
+        } else {
+            // erreur 404
+            header('HTTP/1.0 404 Not Found');
+            echo 'La page n\'existe pas';
+        }
+        exit();
+    }
 }
